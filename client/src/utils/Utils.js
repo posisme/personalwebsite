132d41f2726536c393aafd08d3f605d9d05bca79
Randy Pospisil <posisme@gmail.com>
2026-01-08 16:15:38 +0000

    move to session auth

--- client/src/utils/Utils.js
+++ client/src/utils/Utils.js
@@ -5,14 +5,14 @@ import { map } from 'unist-util-map';
 const mdredo = () => {
 
   const isDirectiveNode = (node) => {
-    const { type,name } = node;
-    if(name && name.match(/^[0-9]/)){
+    const { type, name } = node;
+    if (name && name.match(/^[0-9]/)) {
       return false;
     }
     return type === 'textDirective' || type === 'leafDirective' || type === 'containerDirective';
   };
   const mapDirectiveNode = (node) => {
-    
+
     if (isDirectiveNode(node)) {
       const { properties, tagName } = h(node.name, node.attributes);
       return Object.assign(Object.assign({}, node), {
@@ -24,117 +24,117 @@ const mdredo = () => {
     }
     if (node.name && node.name.match(/^[0-9]/)) {
       let newnode = {
-        type:"text",
-        value:":"+node.name,
-        position:node.position
+        type: "text",
+        value: ":" + node.name,
+        position: node.position
       }
       return newnode;
     }
     return node;
   };
-  
+
   const transformNodeTree = (nodeTree) => map(nodeTree, mapDirectiveNode);
   const remarkDirectiveRehype = () => transformNodeTree;
-  
-  
+
+
   return remarkDirectiveRehype();
-  }
+}
 
 const Utils = {
 
 
-    isMobile: () => {
-        const userAgent = navigator.userAgent;
-        return /Android|iOS|Mobile/i.test(userAgent);
-    },
-    useToken: () => {
-      // Initialize loginmessage once
-      const [loginmessage, setMessage] = useState("Not Logged In");
-    
-      const getToken = () => {
-        const tokenString = sessionStorage.getItem('token');
-        const userToken = tokenString ? JSON.parse(tokenString) : null;
-        return userToken;
-      };
-    
-      const [token, setToken] = useState(getToken());
-    
-      const saveToken = (userToken) => {
-        if (userToken && userToken.name) {
-          sessionStorage.setItem('token', JSON.stringify(userToken));
-          setToken(userToken);
-          setMessage("yep");
-          return; // Exit the function after setting "yep"
-        }
-        setMessage("Wrong Credentials. Not logged in.");
-      };
-    
-      return {
-        setToken: saveToken,
-        token,
-        loginmessage
-      };
-    },
-    yMD: (startdate,enddate) =>{
-      var retarr = [];
-      let years,months,days;
-      var startdate = new Date(startdate);
-      var today = new Date(enddate?enddate:new Date());
-      if(startdate < today){
-        years = today.getFullYear() - startdate.getFullYear();
-        months = today.getMonth() - startdate.getMonth();
-        days = today.getDate() - startdate.getDate();
-        
-      }
-      else{
-        years = startdate.getFullYear() - today.getFullYear();
-        months = startdate.getMonth() - today.getMonth();
-        days = startdate.getDate() - today.getDate();
-      }
-      if(days < 0){
-            const lastMonth = new Date(today.getFullYear(),today.getMonth(),0);
-            days += lastMonth.getDate();
-            months--;
-        }
-        if(months < 0){
-            months += 12;
-            years--;
-        }
-      
-      var ret =  [
-        years>1?years + " years":(years == 1?years + " year":null),
-        months>1?months + " months":(months == 1?months + " month":null),
-        days>1?days + " days":(days == 1? days + " day":null),
-      ].filter((j)=>j != null);
-      var last = ret.pop();
-      return ret.join(", ")+" and "+ last;
-      
-    },
-    levenshteinDistance: (s1,s2) =>{
-      const m = s1.length;
-      const n = s2.length;
-      const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(null));
+  isMobile: () => {
+    const userAgent = navigator.userAgent;
+    return /Android|iOS|Mobile/i.test(userAgent);
+  },
+  useToken: () => {
+    // Initialize loginmessage once
+    const [loginmessage, setMessage] = useState("Not Logged In");
 
-      for (let i = 0; i <= m; i++) {
-          dp[i][0] = i;
-      }
-      for (let j = 0; j <= n; j++) {
-          dp[0][j] = j;
-      }
+    const getToken = () => {
+      const tokenString = localStorage.getItem('token');
+      const userToken = tokenString ? JSON.parse(tokenString) : null;
+      return userToken;
+    };
 
-      for (let i = 1; i <= m; i++) {
-          for (let j = 1; j <= n; j++) {
-              const cost = (s1[i - 1] === s2[j - 1]) ? 0 : 1;
-              dp[i][j] = Math.min(
-                  dp[i - 1][j] + 1,      // Deletion
-                  dp[i][j - 1] + 1,      // Insertion
-                  dp[i - 1][j - 1] + cost // Substitution
-              );
-          }
+    const [token, setToken] = useState(getToken());
+
+    const saveToken = (userToken) => {
+      if (userToken && userToken.name) {
+        localStorage.setItem('token', JSON.stringify(userToken));
+        setToken(userToken);
+        setMessage("yep");
+        return; // Exit the function after setting "yep"
       }
-      return dp[m][n];
-    },
-    remarkDirectiveRehype:mdredo
+      setMessage("Wrong Credentials. Not logged in.");
+    };
+
+    return {
+      setToken: saveToken,
+      token,
+      loginmessage
+    };
+  },
+  yMD: (startdate, enddate) => {
+    var retarr = [];
+    let years, months, days;
+    var startdate = new Date(startdate);
+    var today = new Date(enddate ? enddate : new Date());
+    if (startdate < today) {
+      years = today.getFullYear() - startdate.getFullYear();
+      months = today.getMonth() - startdate.getMonth();
+      days = today.getDate() - startdate.getDate();
+
+    }
+    else {
+      years = startdate.getFullYear() - today.getFullYear();
+      months = startdate.getMonth() - today.getMonth();
+      days = startdate.getDate() - today.getDate();
+    }
+    if (days < 0) {
+      const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+      days += lastMonth.getDate();
+      months--;
+    }
+    if (months < 0) {
+      months += 12;
+      years--;
+    }
+
+    var ret = [
+      years > 1 ? years + " years" : (years == 1 ? years + " year" : null),
+      months > 1 ? months + " months" : (months == 1 ? months + " month" : null),
+      days > 1 ? days + " days" : (days == 1 ? days + " day" : null),
+    ].filter((j) => j != null);
+    var last = ret.pop();
+    return ret.join(", ") + " and " + last;
+
+  },
+  levenshteinDistance: (s1, s2) => {
+    const m = s1.length;
+    const n = s2.length;
+    const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(null));
+
+    for (let i = 0; i <= m; i++) {
+      dp[i][0] = i;
+    }
+    for (let j = 0; j <= n; j++) {
+      dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+        const cost = (s1[i - 1] === s2[j - 1]) ? 0 : 1;
+        dp[i][j] = Math.min(
+          dp[i - 1][j] + 1,      // Deletion
+          dp[i][j - 1] + 1,      // Insertion
+          dp[i - 1][j - 1] + cost // Substitution
+        );
+      }
+    }
+    return dp[m][n];
+  },
+  remarkDirectiveRehype: mdredo
 };
 
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const cors = require('cors');
-const env = require('dotenv').config({path:'../.env'});
+const env = require('dotenv').config({ path: '../.env' });
 const app = express();
 const path = require('path');
 
@@ -13,11 +13,17 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const https = require('https');
 const bearerToken = require('bearer-token');
-const authenticate = (req,res,next)=>{
+const session = require('express-session');
+const SQLiteStore = require('connect-sqlite3')(session);
+const authenticate = (req, res, next) => {
+    if (req.session && req.session.user) {
+        next();
+        return;
+    }
     const origin = req.headers.referer;
     const host = req.headers.host;
-    
-    if(origin && host && (new URL(origin).host === host || new URL(origin).host == "posis.me")){
+
+    if (origin && host && (new URL(origin).host === host || new URL(origin).host == "posis.me")) {
         //console.log("Request from same domain");
         next();
         return;
@@ -31,35 +37,35 @@ const authenticate = (req,res,next)=>{
             return;
         }
     }
-    bearerToken(req,async (err,token)=>{
-        
-        if(err){
-            return res.status(501).json({error:"Auth error."})
+    bearerToken(req, async (err, token) => {
+
+        if (err) {
+            return res.status(501).json({ error: "Auth error." })
         }
-        if(!token){
-            return res.status(401).json({error:"No Token Provided"});
+        if (!token) {
+            return res.status(401).json({ error: "No Token Provided" });
         }
-        if(!await isValidApikey(token)){
-            return res.status(403).json({error:"Invalid API token"});
+        if (!await isValidApikey(token)) {
+            return res.status(403).json({ error: "Invalid API token" });
         }
         next();
-        
-        
+
+
     })
-    
-    
+
+
 }
-const isValidApikey = async (apikey)=>{
-    try{
-        const hashedkeys = await dbrun("select api_key from api_keys",[],"select");
-        for(i=0;i<hashedkeys.length;i++){
-            var b = await bcrypt.compare(apikey,hashedkeys[i].api_key);
-            if(b){
+const isValidApikey = async (apikey) => {
+    try {
+        const hashedkeys = await dbrun("select api_key from api_keys", [], "select");
+        for (i = 0; i < hashedkeys.length; i++) {
+            var b = await bcrypt.compare(apikey, hashedkeys[i].api_key);
+            if (b) {
                 return true;
             }
-        } 
-    } catch(error){
-        console.error("ERROR",error);
+        }
+    } catch (error) {
+        console.error("ERROR", error);
         return false;
     }
     return false;
@@ -67,16 +73,30 @@ const isValidApikey = async (apikey)=>{
 }
 
 
-const {dbrun} = require("./utils/sharedutils");
+const { dbrun } = require("./utils/sharedutils");
 
 
 app.use(express.json());
+
+app.use(session({
+    store: new SQLiteStore({
+        db: 'sessions.db',
+        dir: './db'
+    }),
+    secret: process.env.SESSION_SECRET || 'supersecretkey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false, // Set to true if using HTTPS
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 1 week
+    }
+}));
 
 app.use(authenticate);
 
 
 app.use(cors({
-    origin:'https://posis.me'
+    origin: 'https://posis.me'
 }))
 
 //the nginx redirects anything /api to this servers / endpoint, so though it is going to /api, this
@@ -87,103 +107,115 @@ const mailchimp = require("./routes/mailchimp");
 app.use("/mailchimp", mailchimp);
 
 const meals = require("./routes/meals")
-app.use("/meals",meals);
+app.use("/meals", meals);
 
 const pictures = require("./routes/pictures");
-app.use("/pictures",pictures);
+app.use("/pictures", pictures);
 
 const reading = require("./routes/reading");
-app.use("/reading",reading);
+app.use("/reading", reading);
 
 const docs = require("./routes/docs");
-app.use("/docs",docs);
+app.use("/docs", docs);
 
 // end routes
 
-app.get("/",(req,res)=>{
-    res.json({error:"no api selected"})
+app.get("/", (req, res) => {
+    res.json({ error: "no api selected" })
 })
-app.post("/setlogin", async (req,res)=>{
+app.post("/setlogin", async (req, res) => {
     var ret;
     bcrypt.genSalt(saltRounds, (err, salt) => {
         if (err) {
-          // Handle error
-          return;
+            // Handle error
+            return;
         }
-        bcrypt.hash(req.body.password, salt,( err,hash)=>{
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
 
-            if(err){
+            if (err) {
                 console.log("error in password", err);
-                res.send({"d":"error"});
+                res.send({ "d": "error" });
             }
             dbrun("insert into authusers (username,hashepassword,name,email) values (?,?,?,?)",
-                [  
+                [
                     req.body.username,
                     hash,
                     req.body.name,
                     req.body.email
                 ],
                 "insert");
-            res.send({"d":"updated"});
+            res.send({ "d": "updated" });
         });
-      });
-    
-    
+    });
+
+
 })
-app.post("/setapikey", async (req,res)=>{
+app.post("/setapikey", async (req, res) => {
     var ret;
     console.log(req.body);
     bcrypt.genSalt(saltRounds, (err, salt) => {
         if (err) {
-          // Handle error
-          return;
+            // Handle error
+            return;
         }
-        bcrypt.hash(req.body.apikey, salt,( err,hash)=>{
+        bcrypt.hash(req.body.apikey, salt, (err, hash) => {
 
-            if(err){
+            if (err) {
                 console.log("error in password", err);
-                res.send({"d":"error"});
+                res.send({ "d": "error" });
             }
             dbrun("insert into api_keys (api_key) values (?)",
-                [  
+                [
                     hash
                 ],
                 "insert");
-            res.send({"d":"updated"});
+            res.send({ "d": "updated" });
         });
-      });
-    
-    
+    });
+
+
 })
-app.use('/login', async (req,res)=>{
+app.use('/login', async (req, res) => {
     var ret = "";
-    var user = await dbrun("select username,hashepassword as h,name,email  from authusers where username = ?",[req.body.username],"select")
-    if(user && user.length){
-        bcrypt.compare(req.body.password,user[0].h,(err,result)=>{
-            if(err){
-                console.log("error in compare",err);
-                res.send({"user":null})
+    var user = await dbrun("select username,hashepassword as h,name,email  from authusers where username = ?", [req.body.username], "select")
+    if (user && user.length) {
+        bcrypt.compare(req.body.password, user[0].h, (err, result) => {
+            if (err) {
+                console.log("error in compare", err);
+                res.send({ "user": null })
                 return;
             }
-            if(result){
+            if (result) {
                 console.log("Passwords match");
-                res.send({"name":user[0].name,"email":user[0].email});
+                req.session.user = user[0];
+                res.send({ "name": user[0].name, "email": user[0].email });
             }
-            else{
+            else {
                 console.log("Passwords don't match");
-                res.send({"user":null})
+                res.send({ "user": null })
             }
         })
     }
-    else{
+    else {
         console.log("no user found");
-        res.send({"user":null})
+        res.send({ "user": null })
     }
 });
 
 
 
 
+
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Error destroying session:", err);
+            return res.status(500).send({ error: 'Logout failed' });
+        }
+        res.clearCookie('connect.sid');
+        res.send({ message: 'Logged out successfully' });
+    });
+});
 
 app.listen(6125, () => {
     console.log(`running on port 6125...`);
